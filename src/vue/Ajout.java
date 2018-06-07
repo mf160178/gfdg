7ec6ff73ae25af0c46ef8feb06697a54bb50e894
mf160178 <Marine.foucambert@edu.ece.fr>
2018-06-07 16:29:57 +0200

    Stp

--- src/vue/Ajout.java
+++ src/vue/Ajout.java
@@ -5,43 +5,66 @@
  */
 package vue;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.Frame;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 /**
  *
  * @author FionaC
  */
-public class Ajout   {
+public class Ajout {
+
+    String[] buttons = {"Fond", "Instrument"};
+    int choix;
+    JOptionPane nomDialog = new JOptionPane();
+    JOptionPane valeurDialog = new JOptionPane();
+    JOptionPane affichage = new JOptionPane();
+    String nom;
+    double valeur;
 
     public Ajout() {
- String[] buttons = { "Fond", "Instrument" };
 
-    int choix = JOptionPane.showOptionDialog(null, "Ajouter quel element?", "Ajout",
-        JOptionPane.WARNING_MESSAGE, 0, null, buttons, buttons[1]);
+        choix = JOptionPane.showOptionDialog(null, "Ajouter quel element?", "Ajout",
+                JOptionPane.WARNING_MESSAGE, 0, null, buttons, buttons[1]);
 
-    System.out.println(choix);
-    //faire condition 0 ou 1
-    
-        JOptionPane nomDialog = new JOptionPane();
-        JOptionPane valeurDialog = new JOptionPane();
-        JOptionPane affichage = new JOptionPane();
-    String nom = nomDialog.showInputDialog(null, "Entrez la clé", "Clé element", JOptionPane.QUESTION_MESSAGE);
-    //verifier si deja existant
-    double valeur =Double.valueOf(valeurDialog.showInputDialog(null, "Entrez la valeur", "Valeur element", JOptionPane.QUESTION_MESSAGE));
-    // verifier si positif
-    affichage.showMessageDialog(null, "La clé est " + nom + "\n Valeur: "+ valeur, "Infos de l'ajout", JOptionPane.INFORMATION_MESSAGE);
-    
-
+        System.out.println(choix);
         
+        if (choix==0)
+            ajoutFond();
+        else if (choix==1)
+            ajoutInstrument();
+       
     }
+public void ajoutFond(){
+     nom = nomDialog.showInputDialog(null, "Entrez la clé", "Clé fond", JOptionPane.QUESTION_MESSAGE);
+        //verifier si deja existant
+        //        //Demande de fond à l'utilisateur
+//        System.out.println("***** Question 1.5 : ajout d'un nouveau fond *****");
+//        double amount1 = 0;
+//        do {
+//            System.out.print("Indiquer le montant (positif) d'un fond: ");
+//            amount1 = scan.nextDouble();
+//        } while (amount1 < 0);
+//        System.out.print("Indiquer la clé de ce fond: ");
+//        String key1 = scan.next();
+//        Fonds fond1 = new Fonds(amount1, key1);
+//        
+//        //On recherche le fond entré par l'utilisateur, s'il n'existe pas, on l'ajoute à la Map des fonds du portefeuille
+//        try {
+//            double testAmount = por.searchFonds(key1);
+//            System.out.println("Ce fond existe déjà, vous ne pouvez pas l'ajouter!");
+//        } catch (FondsInexistants ex) {
+//            por.createFonds(key1, amount1); //Le fond entré n'existe pas, on l'ajoute 
+//        }
+        valeur = Double.valueOf(valeurDialog.showInputDialog(null, "Entrez la valeur", "Valeur fond", JOptionPane.QUESTION_MESSAGE));
+        // verifier si positif
+        affichage.showMessageDialog(null, "La clé est " + nom + "\nValeur: " + valeur, "Infos de l'ajout", JOptionPane.INFORMATION_MESSAGE);
 
+    
+}
+public void ajoutInstrument(){
+    nom = nomDialog.showInputDialog(null, "Entrez la clé", "Clé instrument", JOptionPane.QUESTION_MESSAGE);
+        //verifier si deja existant
+                affichage.showMessageDialog(null, "La clé est " + nom, "Infos de l'ajout", JOptionPane.INFORMATION_MESSAGE);
+
+}
 }
